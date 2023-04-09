7de11f4b878a35acd412737d22dfcff9060b1f24
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2023-04-09 21:10:23 +0530

    Commit to Land A Dream Job in CSE :)

---
+++ standard.java
@@ -0,0 +1,23 @@
+package pakka;
+
+public class standard {
+
+	public static void main(String args[])
+	{
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		System.out.println("***\\VAIBHAV PHODEGA 12 LPA k PACKAGE//***");
+		System.out.println("*****");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+
+	}
+	
+	
+	
+}
