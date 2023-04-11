cde395c3201849a0a5fe786cbc43876ab7ca05ea
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2023-04-11 21:46:48 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,12 @@
+package pakka;
+
+public class Pattern1 {
+      public static void main(String args[]) {
+    	  for(int row=0; row<5; row++)
+    	  {
+    		  for(int col=0; col<5; col++)
+    		  System.out.print("3* ");
+    		  System.out.println();
+    	  }
+      }
+}
