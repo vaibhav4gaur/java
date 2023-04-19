1908d665846bbfb9ad098d2be988cf5bec8391e3
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2023-04-19 19:19:51 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+package pakka;
+
+class Employee{
+	
+	static int empID=2;
+	
+	
+}
+
+
+
+ class Test {
+     public static void main(String[] args)
+     {
+    	 System.out.println(Employee.empID);
+     }
+}
